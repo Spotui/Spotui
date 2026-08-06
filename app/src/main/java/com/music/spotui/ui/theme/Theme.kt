3d8f70115ea16c317ecf3d401d390c602a592442
Felixz101 <Felixz101@users.noreply.github.com>
2026-08-06 17:51:39 +0800

    fix: use white status bar icons for better contrast

--- app/src/main/java/com/music/spotui/ui/theme/Theme.kt
+++ app/src/main/java/com/music/spotui/ui/theme/Theme.kt
@@ -62,7 +62,7 @@ fun SpotuiTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Color.Transparent.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
